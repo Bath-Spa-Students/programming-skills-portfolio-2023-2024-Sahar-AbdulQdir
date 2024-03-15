5e2002bde9e4124c9075e7f3aa23a696a0b9ae64
sahar <sahaxar11@gmail.com>
2024-03-15 15:26:17 +0400

    Create Exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise3.py
@@ -0,0 +1,13 @@
+# declaring a var and printing it
+name = "\tMona\n\n\nAli\t"
+print(name)
+
+#using the 3 stripping functions and printing it
+name_strip = name.strip()
+print(name_strip)
+
+name_rstrip = name.rstrip()
+print(name_rstrip)
+
+name_Istrip = name.rstrip()
+print(name_Istrip)
