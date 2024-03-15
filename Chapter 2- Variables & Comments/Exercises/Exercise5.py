964d534fe0d9f67abf733c9184346466b7ff6ac0
sahar <sahaxar11@gmail.com>
2024-03-15 15:26:27 +0400

    Create Exercise5.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise5.py
@@ -0,0 +1,23 @@
+#declaring variables
+Budget = 50
+USB_price = 6
+
+#declaring a var to calculata how many USB she can buy
+USB_num = 50 // 6
+
+#declaring var with a massege 
+massege = ("The number of USB sticks she can buy is" + " " + str(USB_num) + " " + "sticks.")
+
+#printing that massege
+print(massege)
+
+#declaring a var to calculata how many pounds she will have
+pounds_left = 50 % 6
+
+#declaring var with a massege 
+massege1 = ("She will have" + " " + str(pounds_left) + " "+ "pounds left.")
+
+#printing that massege
+print(massege1)
+
+
