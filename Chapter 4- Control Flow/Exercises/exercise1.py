38cd10f0279acce4ffbc0d7c81d3aca1afb752f0
sahar <sahaxar11@gmail.com>
2024-04-02 11:14:29 +0400

    chapter4.py

--- Chapter 4- Control Flow/Exercises/exercise1.py
+++ Chapter 4- Control Flow/Exercises/exercise1.py
@@ -3,11 +3,11 @@ alien_color = "green"
 
 #using if statement to see if the player win
 if alien_color == "green":
-    print("Congratulations, you just earned 5 ponts")
+    print("Congratulations, you just earned 5 points")
 
 #declaring a var with the color red
 alien_color = "red"
 #using another if statement to see if choose another color
 if alien_color == "green":
-    print("Congratulations, you just earned 5 ponts")
+    print("Congratulations, you just earned 5 points")
 
